7e689d17c9749def5d6b135dc60abbd9230a485f
annhoang191 <hquynhanh19@gmail.com>
2019-03-12 15:07:34 +0700

    Setting static assets on production env

--- sample_app/settings.py
+++ sample_app/settings.py
@@ -18,6 +18,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+  'whitenoise.middleware.WhiteNoiseMiddleware',
   'django.middleware.security.SecurityMiddleware',
   'django.contrib.sessions.middleware.SessionMiddleware',
   'django.middleware.common.CommonMiddleware',
@@ -83,9 +84,18 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATIC_URL = '/static/'
-ALLOWED_HOSTS = ['djangosampleapp.herokuapp.com']
+ALLOWED_HOSTS = ['djangosampleapp.herokuapp.com', 'localhost']
 
 import dj_database_url
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
+
+PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra lookup directories for collectstatic to find static files
+STATICFILES_DIRS = (
+  os.path.join(PROJECT_ROOT, 'static'),
+)
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
